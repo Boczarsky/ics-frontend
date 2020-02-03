375e73d807f2f0f296fc1309875abdb9c2946ef4
Tomasz Boczarski <boczarskitomasz@gmail.com>
2020-02-03 19:11:09 +0100

    connecting to api

--- src/components/IcecreamShops/index.tsx
+++ src/components/IcecreamShops/index.tsx
@@ -6,16 +6,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import ShopOverview from '../common/ShopOverview';
 import { fetchFavoriteShops } from '../../reducers/FavoriteShops/operations';
 import { fetchOwnedShops } from '../../reducers/OwnedShops/operations';
+import userType from '../../enums/userType';
 
 const IcecreamShops = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchFavoriteShops());
-    dispatch(fetchOwnedShops());
-  }, [dispatch])
 
   const favorites = useSelector((state:any) => state.favoriteShops.list);
   const owned = useSelector((state:any) => state.ownedShops.list);
+  const uType = useSelector((state: any) => Number(state.auth.userType));
+  const uId = useSelector((state: any) => state.auth.userId);
+
+  useEffect(() => {
+    if (uType === userType.manager || uType === userType.employee) {
+      dispatch(fetchOwnedShops(uType, uId));
+    }
+    if (uType === userType.client) {
+      dispatch(fetchFavoriteShops());
+    }
+  }, [dispatch, uType, uId])
   
   const history = useHistory();
   const handleOwnedClick = (data: any) => {
@@ -32,12 +40,17 @@ const IcecreamShops = () => {
       }
     >
       <div className="icecream-shops">
-        <div className="icecream-shops__owned-list">
+        {userType.client === uType && <div className="icecream-shops__favorite-list">
           {favorites.map((favoriteShop: any) => <ShopOverview key={`favorite-${favoriteShop.id}`} data={favoriteShop} handleClick={handleFavoriteClick}/>)}
-        </div>
-        <div className="icecream-shops__favorite-list">
-          <Link className="icecream-shops__create" to="/icecream-shops/create"><div className="b-button clickable">Create icecream shop</div></Link>
+        </div>}
+        <div className="icecream-shops__owned-list">
+          {userType.manager === uType && <>
+          <Link className="icecream-shops__create" to="/icecream-shops/create"><div className="b-button clickable">Create icecream shop</div></Link>}
           {owned.map((ownedShop: any) => <ShopOverview key={`owned-${ownedShop.id}`} data={ownedShop} handleClick={handleOwnedClick}/>)}
+          </>}
+          {userType.employee === uType && <>
+          {owned.map((ownedShop: any) => <ShopOverview key={`owned-${ownedShop.id}`} data={ownedShop} handleClick={handleFavoriteClick}/>)}
+          </>}
         </div>
       </div>
     </AppLayout>
--- src/reducers/OwnedShops/operations.ts
+++ src/reducers/OwnedShops/operations.ts
@@ -1,23 +1,28 @@
+import { generateUrl } from './../../components/common/UploadImage/index';
 import { fetchIcecreamShopsStart, fetchIcecreamShopsSuccess } from './actions';
-export const fetchOwnedShops = () => (dispatch: Function, getState: Function) => {
+import { dataProvider } from '../../utils/requestBuilder';
+import userType from '../../enums/userType';
+export const fetchOwnedShops = (uType: number, userId: number) => (dispatch: Function, getState: Function) => {
   dispatch(fetchIcecreamShopsStart());
-  const icecreamShops = [
-    {
-      id: 1,
-      name: 'Cool Icecream Shop',
-      logoUrl: '',
-      address: 'Króla Jankiela II 16/1, 44-100 Gliwice',
-      flavours: [],
-      follows: 1519
-    },
-    {
-      id: 2,
-      name: 'Cooler Icecream Shop',
-      logoUrl: '',
-      address: 'Króla Jankiela II 16/2, 44-100 Gliwice',
-      flavours: [],
-      follows: 5161
-    }
-  ];
-  dispatch(fetchIcecreamShopsSuccess(icecreamShops));
+  let data;
+  if (userType.manager === uType) {
+    data = {managerId: userId};
+  }
+  if (userType.employee === uType) {
+    data = {employeeId: userId};
+  }
+  dataProvider().post('icecream-shops/list', {offset: 0, limit: 999, ...data})
+    .then((response) => {
+      dispatch(fetchIcecreamShopsSuccess(response.data.result.map((ics: any) => ({
+        id: ics.icecream_shop_id,
+        name: ics.name,
+        logoUrl: ics.logo_file_name && generateUrl(ics.logo_file_name),
+        address: `${ics.street}, ${ics.postal_code} ${ics.city}`,
+        flavours: [],
+        follows: Number(ics.follows),
+      }))));
+    })
+    .catch((error) => {
+      console.error(error);
+    })
 };
